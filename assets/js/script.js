2dee836caed4e620da45059a0f7e4bc16df2c6ac
DaveSalterM <158429937+DaveSalterM@users.noreply.github.com>
2024-03-01 18:28:51 -0800

    styling/functinality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,53 @@
 // ? Grab references to the important DOM elements.
 
-const projectDisplayEl = $('#project-display');
-const projectFormEl = $('#task-form');
-const projectNameInputEl = $('#task-name-input');
-const projectTypeInputEl = $('#task-description');
-const projectDateInputEl = $('#taskDueDate');
+const taskDisplayEl = $('#task-display');
+const taskFormEl = $('#task-form');
+const taskNameInputEl = $('#task-name-input');
+const taskTypeInputEl = $('#task-description');
+const taskDateInputEl = $('#taskDueDate');
 
 
 
-// ? Reads projects from local storage and returns array of project objects.
-// ? If there are no projects in localStorage, it initializes an empty array ([]) and returns it.
-function readProjectsFromStorage() {
-  // ? Retrieve projects from localStorage and parse the JSON to an array.
-  // ? We use `let` here because there is a chance that there are no projects in localStorage (which means the projects variable will be equal to `null`) and we will need it to be initialized to an empty array.
-  let projects = JSON.parse(localStorage.getItem('projects'));
+// ? Reads tasks from local storage and returns array of task objects.
+// ? If there are no tasks in localStorage, it initializes an empty array ([]) and returns it.
+function readTasksFromStorage() {
+  // ? Retrieve tasks from localStorage and parse the JSON to an array.
+  // ? We use `let` here because there is a chance that there are no tasks in localStorage (which means the tasks variable will be equal to `null`) and we will need it to be initialized to an empty array.
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
 
-  // ? If no projects were retrieved from localStorage, assign projects to a new empty array to push to later.
-  if (!projects) {
-    projects = [];
+  // ? If no tasks were retrieved from localStorage, assign tasks to a new empty array to push to later.
+  if (!tasks) {
+    tasks = [];
   }
 
-  // ? Return the projects array either empty or with data in it whichever it was determined to be by the logic right above.
-  return projects;
+  // ? Return the tasks array either empty or with data in it whichever it was determined to be by the logic right above.
+  return tasks;
 }
 
-// ? Accepts an array of projects, stringifys them, and saves them in localStorage.
-function saveProjectsToStorage(projects) {
-  localStorage.setItem('projects', JSON.stringify(projects));
+// ? Accepts an array of tasks, stringifys them, and saves them in localStorage.
+function saveTasksToStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// ? Creates a project card from the information passed in `project` parameter and returns it.
-function createProjectCard(project) {
+// ? Creates a task card from the information passed in `task` parameter and returns it.
+function createTaskCard(task) {
   const taskCard = $('<div>')
-    .addClass('card project-card draggable my-3')
-    .attr('data-project-id', project.id);
-  const cardHeader = $('<div>').addClass('card-header h4').text(project.name);
+    .addClass('card task-card draggable my-3')
+    .attr('data-task-id', task.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
   const cardBody = $('<div>').addClass('card-body');
-  const cardDescription = $('<p>').addClass('card-text').text(project.type);
-  const cardDueDate = $('<p>').addClass('card-text').text(project.dueDate);
+  const cardDescription = $('<p>').addClass('card-text').text(task.type);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
   const cardDeleteBtn = $('<button>')
     .addClass('btn btn-danger delete')
     .text('Delete')
-    .attr('data-project-id', project.id);
-  cardDeleteBtn.on('click', handleDeleteProject);
+    .attr('data-task-id', task.id);
+  cardDeleteBtn.on('click', handleDeleteTask);
 
   // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
-  if (project.dueDate && project.status !== 'done') {
+  if (task.dueDate && task.status !== 'done') {
     const now = dayjs();
-    const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
 
     // ? If the task is due today, make the card yellow. If it is overdue, make it red.
     if (now.isSame(taskDueDate, 'day')) {
@@ -66,10 +66,10 @@ function createProjectCard(project) {
   return taskCard;
 }
 
-function printProjectData() {
-  const projects = readProjectsFromStorage();
+function printTaskData() {
+  const tasks = readTasksFromStorage();
 
-  // ? Empty existing project cards out of the lanes
+  // ? Empty existing task cards out of the lanes
   const todoList = $('#todo-cards');
   todoList.empty();
 
@@ -79,14 +79,14 @@ function printProjectData() {
   const doneList = $('#done-cards');
   doneList.empty();
 
-  // ? Loop through projects and create project cards for each status
-  for (let project of projects) {
-    if (project.status === 'to-do') {
-      todoList.append(createProjectCard(project));
-    } else if (project.status === 'in-progress') {
-      inProgressList.append(createProjectCard(project));
-    } else if (project.status === 'done') {
-      doneList.append(createProjectCard(project));
+  // ? Loop through tasks and create task cards for each status
+  for (let task of tasks) {
+    if (task.status === 'to-do') {
+      todoList.append(createTaskCard(task));
+    } else if (task.status === 'in-progress') {
+      inProgressList.append(createTaskCard(task));
+    } else if (task.status === 'done') {
+      doneList.append(createTaskCard(task));
     }
   }
 
@@ -108,94 +108,94 @@ function printProjectData() {
   });
 }
 
-// ? Removes a project from local storage and prints the project data back to the page
-function handleDeleteProject() {
-  const projectId = $(this).attr('data-project-id');
-  const projects = readProjectsFromStorage();
+// ? Removes a task from local storage and prints the task data back to the page
+function handleDeleteTask() {
+  const taskId = $(this).attr('data-task-id');
+  const tasks = readTasksFromStorage();
 
-  // ? Remove project from the array. There is a method called `filter()` for this that is better suited which we will go over in a later activity. For now, we will use a `forEach()` loop to remove the project.
-  projects.forEach((project) => {
-    if (project.id === projectId) {
-      projects.splice(projects.indexOf(project), 1);
+  // ? Remove task from the array. There is a method called `filter()` for this that is better suited which we will go over in a later activity. For now, we will use a `forEach()` loop to remove the task.
+  tasks.forEach((task) => {
+    if (task.id === taskId) {
+      tasks.splice(tasks.indexOf(task), 1);
     }
   });
 
-  // ? We will use our helper function to save the projects to localStorage
-  saveProjectsToStorage(projects);
+  // ? We will use our helper function to save the tasks to localStorage
+  saveTasksToStorage(tasks);
 
-  // ? Here we use our other function to print projects back to the screen
-  printProjectData();
+  // ? Here we use our other function to print tasks back to the screen
+  printTaskData();
 }
 
-// ? Adds a project to local storage and prints the project data
-function handleProjectFormSubmit(event) {
+// ? Adds a task to local storage and prints the task data
+function handleTaskFormSubmit(event) {
   event.preventDefault();
 
   // ? Read user input from the form
-  const projectName = projectNameInputEl.val().trim();
-  const projectType = projectTypeInputEl.val(); // don't need to trim select input
-  const projectDate = projectDateInputEl.val(); // yyyy-mm-dd format
+  const taskName = taskNameInputEl.val().trim();
+  const taskType = taskTypeInputEl.val(); // don't need to trim select input
+  const taskDate = taskDateInputEl.val(); // yyyy-mm-dd format
 
-  const newProject = {
-    // ? Here we use a Web API called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
+  const newTask = {
+    // ? Here we use a Web API called `crypto` to generate a random id for our task. This is a unique identifier that we can use to find the task in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
     id:crypto.randomUUID(),
-    name: projectName,
-    type: projectType,
-    dueDate: projectDate,
+    name: taskName,
+    type: taskType,
+    dueDate: taskDate,
     status: 'to-do',
   };
 
-  // ? Pull the projects from localStorage and push the new project to the array
-  const projects = readProjectsFromStorage();
-  projects.push(newProject);
+  // ? Pull the tasks from localStorage and push the new task to the array
+  const tasks = readTasksFromStorage();
+  tasks.push(newTask);
 
-  // ? Save the updated projects array to localStorage
-  saveProjectsToStorage(projects);
+  // ? Save the updated tasks array to localStorage
+  saveTasksToStorage(tasks);
 
-  // ? Print project data back to the screen
-  printProjectData();
+  // ? Print task data back to the screen
+  printTaskData();
 
   // ? Clear the form inputs
-  projectNameInputEl.val('');
-  projectTypeInputEl.val('');
-  projectDateInputEl.val('');
+  taskNameInputEl.val('');
+  taskTypeInputEl.val('');
+  taskDateInputEl.val('');
 }
 
-// ? This function is called when a card is dropped into a lane. It updates the status of the project and saves it to localStorage. You can see this function is called in the `droppable` method below.
+// ? This function is called when a card is dropped into a lane. It updates the status of the task and saves it to localStorage. You can see this function is called in the `droppable` method below.
 function handleDrop(event, ui) {
-  // ? Read projects from localStorage
-  const projects = readProjectsFromStorage();
+  // ? Read tasks from localStorage
+  const tasks = readTasksFromStorage();
 
-  // ? Get the project id from the event
-  const taskId = ui.draggable[0].dataset.projectId;
+  // ? Get the task id from the event
+  const taskId = ui.draggable[0].dataset.taskId;
 
   // ? Get the id of the lane that the card was dropped into
   const newStatus = event.target.id;
 
-  for (let project of projects) {
-    // ? Find the project card by the `id` and update the project status.
-    if (project.id === taskId) {
-      project.status = newStatus;
+  for (let task of tasks) {
+    // ? Find the task card by the `id` and update the task status.
+    if (task.id === taskId) {
+      task.status = newStatus;
     }
   }
-  // ? Save the updated projects array to localStorage (overwritting the previous one) and render the new project data to the screen.
-  localStorage.setItem('projects', JSON.stringify(projects));
-  printProjectData();
+  // ? Save the updated tasks array to localStorage (overwritting the previous one) and render the new task data to the screen.
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  printTaskData();
 }
 
-// ? Add event listener to the form element, listen for a submit event, and call the `handleProjectFormSubmit` function.
-projectFormEl.on('submit', handleProjectFormSubmit);
+// ? Add event listener to the form element, listen for a submit event, and call the `handleTaskFormSubmit` function.
+taskFormEl.on('submit', handleTaskFormSubmit);
 
 // ? Because the cards are dynamically added to the screen, we have to use jQuery event delegation to listen for clicks on the added cards delete button.
-// ? We listen for a click on the parent element, and THEN check if the target of the click is the delete button. If it is, we call the `handleDeleteProject` function
-projectDisplayEl.on('click', '.btn-delete-project', handleDeleteProject);
+// ? We listen for a click on the parent element, and THEN check if the target of the click is the delete button. If it is, we call the `handleDeleteTask` function
+taskDisplayEl.on('click', '.btn-delete-task', handleDeleteTask);
 
 
 
-// ? When the document is ready, print the project data to the screen and make the lanes droppable. Also, initialize the date picker.
+// ? When the document is ready, print the task data to the screen and make the lanes droppable. Also, initialize the date picker.
 $(document).ready(function () {
-  // ? Print project data to the screen on page load if there is any
-  printProjectData();
+  // ? Print task data to the screen on page load if there is any
+  printTaskData();
 
   $('#taskDueDate').datepicker({
     changeMonth: true,
